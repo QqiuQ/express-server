9df7c00b0869c9f16709a3ae8c060953ae663e57
Brant Allen <83905796+vibrantbobo@users.noreply.github.com>
2023-11-09 22:29:03 +0800

    feat. DeliveryController

--- src/main/java/com/team24/express/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/team24/express/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.team24.express.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.team24.express.entity.Account;
-import com.team24.express.entity.Employee;
-import com.team24.express.entity.User;
+import com.team24.express.common.AccountConst;
+import com.team24.express.common.RoleConst;
+import com.team24.express.entity.*;
+import com.team24.express.mapper.AccountRoleMapper;
 import com.team24.express.mapper.EmployeeMapper;
+import com.team24.express.mapper.RoleMapper;
 import com.team24.express.service.EmployeeService;
 import jakarta.annotation.Resource;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,6 +27,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     EmployeeMapper employeeMapper;
     @Resource
     PasswordEncoder passwordEncoder;
+
+    @Resource
+    RoleMapper roleMapper;
+
+    @Resource
+    AccountRoleMapper accountRoleMapper;
 
     /**
      * QueryWrapper 写法
@@ -73,7 +81,7 @@ public class EmployeeServiceImpl implements EmployeeService {
             wrapper.eq("status", employee.getStatus());
         }
         if (employee.getAddress() != null && !employee.getAddress().isEmpty()) {
-            wrapper.eq("address", employee.getAddress());
+            wrapper.like("address", employee.getAddress());
         }
         if (employee.getHireDate() != null) {
             wrapper.ge("hire_date", employee.getAddress());
@@ -86,7 +94,13 @@ public class EmployeeServiceImpl implements EmployeeService {
     public Boolean add(Employee employee) {
         employee.setCreateTime(LocalDateTime.now());
         employee.setPassword(passwordEncoder.encode(employee.getPassword()));
-        if (employeeMapper.insert(employee) > 0) return true;
+        if (employeeMapper.insert(employee) > 0) {
+            // 添加角色关联
+            Employee insertEmp = this.selectByUsername(employee.getUsername());
+            Role userRole = roleMapper.getRoleByRoleName(RoleConst.EMPLOYEE);
+            AccountRole accountRole = new AccountRole(insertEmp.getId(), userRole.getId(), AccountConst.TYPE_USER);
+            return accountRoleMapper.insert(accountRole) > 0;
+        }
         return false;
     }
 
